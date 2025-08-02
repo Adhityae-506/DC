893f0728b97e54d6ec6491bad15f98f904bc9610
Adhityae-506 <adhityaelangovan2006@gmail.com>
2025-08-02 22:00:52 +0530

    02.08.25

---
+++ getProductSign.c
@@ -0,0 +1,40 @@
+// function getProductSign
+// The function/method getProductSign accepts two arguments – SIZE and arr. The integer SIZE represents the size of the integer array arr.
+
+// Your task:
+// The function/method getProductSign must return the sign of the product of all the integers in the given array.
+
+// If the product is a positive value, return 1
+
+// If the product is a negative value, return -1
+
+// If the product is 0, return 0
+
+// Do not write the main() function as it is already defined.
+
+// Boundary Condition(s):
+// 1 ≤ N ≤ 1000
+
+// -100 ≤ Each integer value ≤ 100
+
+// Example Input/Output 1:
+// Input:
+// 4
+// -1 2 3 -4
+
+// Output:
+// 1
+
+// Explanation:
+// Product = -1 × 2 × 3 × -4 = 24 → positive → return 1
+
+// Example Input/Output 2:
+// Input:
+// 5
+// 10 20 -30 4 5
+
+// Output:
+// -1
+// Explanation:
+// Product is negative → return -1
+
